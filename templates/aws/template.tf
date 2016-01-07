40f8b6b2d36b8eff30b95f5daeef1723b1f69344
Yo Takezawa <takezawa.yo@tis.co.jp>
2016-01-07 13:27:40 +0900

    Migrate pattern to create network for terraform from cloud_conductor_init

---
+++ templates/aws/template.tf
@@ -0,0 +1,79 @@
+variable "subnet_size" {}
+variable "availability_zones" {}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+  count = "${var.subnet_size}"
+  vpc_id = "${aws_vpc.main.id}"
+  availability_zone = "${element(split(", ", var.availability_zones),count.index % length(split(", ", var.availability_zones)))}"
+  cidr_block = "10.0.${count.index + 1}.0/24"
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+
+resource "aws_route_table" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.main.id}"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  count = "${var.subnet_size}"
+  subnet_id = "${element(aws_subnet.main.*.id,count.index)}"
+  route_table_id = "${aws_route_table.main.id}"
+}
+
+resource "aws_security_group" "shared_security_group" {
+  name = "SharedSecurityGroup"
+  description = "Shared security group over all instances in platform/optional pattern"
+  vpc_id = "${aws_vpc.main.id}"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 8501
+    to_port = 8501
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 8300
+    to_port = 8302
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  ingress {
+    from_port = 8500
+    to_port = 8500
+    protocol = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
+output "subnet_ids" {
+  value = "${join(", ", aws_subnet.main.*.id)}"
+}
+
+output "shared_security_group" {
+  value = "${aws_security_group.shared_security_group.id}"
+}
---
+++ templates/openstack/template.tf
@@ -0,0 +1,67 @@
+variable "subnet_size" {}
+variable "network_name" {}
+variable "subnet_name" {}
+variable "router_name" {}
+variable "gateway_id" {}
+
+resource "openstack_networking_network_v2" "main" {
+  name = "${var.network_name}"
+  admin_state_up = "true"
+}
+
+resource "openstack_networking_subnet_v2" "main" {
+  count = "${var.subnet_size}"
+  name = "${var.subnet_name}_${count.index}"
+  network_id = "${openstack_networking_network_v2.main.id}"
+  cidr = "10.0.${count.index + 1}.0/24"
+  ip_version = 4
+}
+
+resource "openstack_networking_router_v2" "main" {
+  name = "${var.router_name}"
+  admin_state_up = "true"
+  external_gateway = "${var.gateway_id}"
+}
+
+resource "openstack_networking_router_interface_v2" "main" {
+  count = "${var.subnet_size}"
+  router_id = "${openstack_networking_router_v2.main.id}"
+  subnet_id = "${element(openstack_networking_subnet_v2.main.*.id,count.index)}"
+}
+
+resource "openstack_compute_secgroup_v2" "shared_security_group" {
+  name = "SharedSecurityGroup"
+  description = "Shared security group over all instances in platform/optional pattern"
+  rule {
+    from_port = 22
+    to_port = 22
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+  }
+  rule {
+    from_port = 8501
+    to_port = 8501
+    ip_protocol = "tcp"
+    cidr = "0.0.0.0/0"
+  }
+  rule {
+    from_port = 8300
+    to_port = 8302
+    ip_protocol = "tcp"
+    cidr = "10.0.0.0/16"
+  }
+  rule {
+    from_port = 8500
+    to_port = 8500
+    ip_protocol = "tcp"
+    cidr = "10.0.0.0/16"
+  }
+}
+
+output "network_id" {
+  value = "${openstack_networking_network_v2.main.id}"
+}
+
+output "shared_security_group" {
+  value = "${openstack_compute_secgroup_v2.shared_security_group.id}"
+}
